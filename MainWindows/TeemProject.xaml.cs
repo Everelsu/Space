2e5482d5c1e7d9513af4e3b6048bf88126d3dc9a
Даниил <chuposhevdaniil2103@gmail.com>
2026-05-04 19:45:54 +0900

    MainWindow(Graph)

---
+++ MainWindows/TeemProject.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Space.MainWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для TeemProject.xaml
+    /// </summary>
+    public partial class TeemProject : Window
+    {
+        public TeemProject()
+        {
+            InitializeComponent();
+        }
+    }
+}
